c7303fbe8fb3ad67fee6b25b0e589e145537196f
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-10 16:35:06 -0600

    Renamed application, added base REST command

--- spring-boot-demo/src/main/java/SpringbootProjectDemo/SpringbootDemoApplication.java
+++ spring-boot-demo/src/main/java/SpringbootProjectDemo/SpringbootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.demo.SpringbootDemo;
+package SpringbootProjectDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-demo/src/test/java/SpringbootProjectDemo/SpringbootDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/SpringbootProjectDemo/SpringbootDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot.demo.SpringbootDemo;
+package SpringbootProjectDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
