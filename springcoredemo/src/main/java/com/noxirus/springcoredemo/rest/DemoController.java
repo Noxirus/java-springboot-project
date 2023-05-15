9824f6fa7259a268ffe0d44de225ea2bc43a23c4
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-15 13:29:50 -0600

    Added scope example to coach

--- springcoredemo/src/main/java/com/noxirus/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/noxirus/springcoredemo/rest/DemoController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // Dependency Injection Reference
     //The qualifier needs to be the name of the class, but in camel case
     @Autowired
-    public DemoController(@Qualifier("trackCoach") Coach theCoach){
+    public DemoController(@Qualifier("trackCoach") Coach theCoach,
+                          @Qualifier("trackCoach") Coach theAnotherCoach){
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     /*Setter injection, name does not need to be specific
@@ -29,5 +32,10 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach = anotherCoach," + (myCoach == anotherCoach);
     }
 }
