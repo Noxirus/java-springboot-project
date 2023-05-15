10382367408d5f019e12835e46620d7edb52fcfa
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-15 13:53:14 -0600

    Added configuration for swimCoach as a Spring Bean

--- springcoredemo/src/main/java/com/noxirus/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/noxirus/springcoredemo/rest/DemoController.java
@@ -14,8 +14,8 @@ public class DemoController {
     // Dependency Injection Reference
     //The qualifier needs to be the name of the class, but in camel case
     @Autowired
-    public DemoController(@Qualifier("trackCoach") Coach theCoach,
-                          @Qualifier("trackCoach") Coach theAnotherCoach){
+    public DemoController(@Qualifier("swimCoach") Coach theCoach,
+                          @Qualifier("swimCoach") Coach theAnotherCoach){
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
         anotherCoach = theAnotherCoach;
