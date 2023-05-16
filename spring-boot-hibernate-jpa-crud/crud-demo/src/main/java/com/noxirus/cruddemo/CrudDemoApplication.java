06ab785dc4ab7e1a8e3e8a30bb4d199ab645cdf7
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-16 15:42:37 -0600

    Added find, update and delete operations

--- spring-boot-hibernate-jpa-crud/crud-demo/src/main/java/com/noxirus/cruddemo/CrudDemoApplication.java
+++ spring-boot-hibernate-jpa-crud/crud-demo/src/main/java/com/noxirus/cruddemo/CrudDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CrudDemoApplication {
 
@@ -17,8 +19,35 @@ public class CrudDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			readStudent(studentDAO, 1);
+			System.out.println("Deleted: " + studentDAO.deleteAll());
 		};
+	}
+
+	private void deleteStudent(StudentDAO studentDAO, Integer id){
+		studentDAO.delete(id);
+	}
+
+	private void updateStudent(StudentDAO studentDAO){
+		int studentId = 1;
+		Student studentToUpdate = studentDAO.findById(studentId);
+		studentToUpdate.setFirstName("Scooby");
+
+		studentDAO.update(studentToUpdate);
+		System.out.println("Updated student: " + studentToUpdate.toString());
+	}
+
+	private void getStudentsByLastName(StudentDAO studentDAO, String lastName){
+		for(var student : studentDAO.findByLastName(lastName)){
+			System.out.println("Found: " + student.toString());
+		}
+	}
+
+	private void getAllStudents(StudentDAO studentDAO){
+		List<Student> students = studentDAO.findAll();
+
+		for(var student : students){
+			System.out.println("Student: " + student.toString());
+		}
 	}
 
 	private void readStudent(StudentDAO studentDAO, Integer id){
--- spring-boot-hibernate-jpa-crud/crud-demo/src/main/java/com/noxirus/cruddemo/dao/StudentDAO.java
+++ spring-boot-hibernate-jpa-crud/crud-demo/src/main/java/com/noxirus/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,15 @@ package com.noxirus.cruddemo.dao;
 
 import com.noxirus.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
     Student findById(Integer id);
+    List<Student> findAll();
+    List<Student> findByLastName(String theLastName);
+    void update(Student theStudent);
+    void delete(Integer id);
+    int deleteAll();
 
 }
