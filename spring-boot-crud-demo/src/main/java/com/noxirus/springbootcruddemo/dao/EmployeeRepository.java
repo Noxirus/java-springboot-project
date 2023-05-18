51b6e9a098fdedc8e6d4b60be06c7000df2adf0b
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-18 16:58:19 -0600

    Changed rest controller to spring boot rest implementation

--- spring-boot-crud-demo/src/main/java/com/noxirus/springbootcruddemo/dao/EmployeeRepository.java
+++ spring-boot-crud-demo/src/main/java/com/noxirus/springbootcruddemo/dao/EmployeeRepository.java
@@ -4,5 +4,7 @@ import com.noxirus.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    //This is handling the DAO
+    //It is also handling the REST Controller
 
 }
