f84c95af799add8f046e0081e77eab795fe67ef6
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-18 17:06:59 -0600

    Added small configuration logic to spring REST

--- spring-boot-crud-demo/src/main/java/com/noxirus/springbootcruddemo/dao/EmployeeRepository.java
+++ spring-boot-crud-demo/src/main/java/com/noxirus/springbootcruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.noxirus.springbootcruddemo.dao;
 
 import com.noxirus.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //This is handling the DAO
     //It is also handling the REST Controller
-
+    //Sorting: http://localhost:8080/magic-api/employees?sort=lastName,desc
 }
