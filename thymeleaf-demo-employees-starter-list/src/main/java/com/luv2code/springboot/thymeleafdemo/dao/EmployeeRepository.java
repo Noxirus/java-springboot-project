7b170fd4157fc014abb023a7b55ee901772a9eb2
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-05-24 13:50:45 -0600

    Added employee input form for updating the DB

--- thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+
+    //Spring data JPA will take this method name and create the endpoint method based on this
+    public List<Employee> findAllByOrderByLastNameAsc();
 	
 }
