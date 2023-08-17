0b1db005fe6e92a481ea6fa0540cf28e551ed51f
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-08-17 15:48:31 -0600

    Added custom annotation with validation rules

---
+++ validationdemo/src/main/java/com/noxirus/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,24 @@
+package com.noxirus.springdemo.mvc.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+    public String value() default "NOX";
+
+    public String message() default "must start with NOX";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+
+}
